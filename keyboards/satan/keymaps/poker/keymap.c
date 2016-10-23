72b956c0702f2d8db81a9cbc4c2ac459a6cc2261
Jasonm23 <jasonm23@gmail.com>
2016-10-23 13:21:19 +0800

    Set Satan/GH60 revCHN to match keymap

--- keyboards/satan/keymaps/poker/keymap.c
+++ keyboards/satan/keymaps/poker/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |FN  |Ctrl |
+   * |Ctrl|Alt |Gui |         Space         |Gui |FN  |Alt |Ctrl |
    * `-----------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
@@ -30,13 +30,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
   KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
   KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSFT, \
-  KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                             KC_RALT, MO(_RL), MO(_FL), KC_RCTL),
+  KC_LCTL, KC_LALT, KC_LGUI,                           KC_SPC,                             KC_RALT, MO(_FL), KC_RALT, KC_RCTL),
 
 /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
    * | ~ | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|  DEL  |
    * |-----------------------------------------------------------|
-   * |     | Up|  |  |  |  |Cal|   |Ins|   |PrSc|Sclk|Paus|      |
+   * |     |  | Up|  |  |  |Cal|   |Ins|   |PrSc|Sclk|Paus|      |
    * |-----------------------------------------------------------|
    * |      |Left|Down|Rig|  |  |  |  |   |   |Home|PgUp|        |
    * |-----------------------------------------------------------|
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP,          KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU,  KC_MUTE, KC_END,  KC_PGDN,                   KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-  
+
   /* Keymap _RL: Function Layer
    * ,-----------------------------------------------------------.
    * |   |   |   |   |   |   |   |   |   |   |   |   |   |  RESET|
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |          |
    * |-----------------------------------------------------------|
    * |    |    |    |                        |    |    |    |    |
-   * `-----------------------------------------------------------'   
+   * `-----------------------------------------------------------'
    */
 [_RL] = KEYMAP_ANSI(
   #ifdef RGBLIGHT_ENABLE
