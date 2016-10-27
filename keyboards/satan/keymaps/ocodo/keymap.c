7077d5d8f86ab33a5fab10cfd098f9cfedfde918
Jasonm23 <jasonm23@gmail.com>
2016-10-27 22:04:19 +0800

    Right hand modifier cluster as arrows and mods

--- keyboards/satan/keymaps/ocodo/keymap.c
+++ keyboards/satan/keymaps/ocodo/keymap.c
@@ -19,6 +19,11 @@ enum macro_id {
   M_SSL
 };
 
+#define UP_T    SFT_T(KC_UP)
+#define DOWN_T  ALT_T(KC_DOWN)
+#define LEFT_T  GUI_T(KC_LEFT)
+#define RIGHT_T CTL_T(KC_RIGHT)
+
 #define JM23  M(M_NOM)
 #define GMAIL M(M_EML)
 #define GHUB  M(M_GHB)
@@ -43,25 +48,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ├─────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─────┤
    * │Bsp/Ctl│  A│  S│  D│  F│  G│  H│  J│  K│  L│  ;│  '│Ret/Ctl│
    * ├───────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───────┤
-   * │Shift   │  Z│  X│  C│  V│  B│  N│  M│  ,│  .│  /│Shift     │
+   * │Shift   │  Z│  X│  C│  V│  B│  N│  M│  ,│  .│  /│Shift/Up  │
    * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴─┬─┴──┬┴───┼────┬─────┤
-   * │Ctrl│Alt │Gui │         Space         │Gui │FN  │Alt │Ctrl │
+   * │Ctrl│Alt │Gui │         Space         │Fn  │Lt/G│Dn/A│Rt/Ct│
    * ╰────┴────┴────┴───────────────────────┴────┴────┴────┴─────╯
    */
 [_BL] = KEYMAP_ANSI(
   F(0),    KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
   BSP_CTL, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          RET_CTL,  \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSFT, \
-  KC_LCTL, KC_LALT, KC_LGUI,                           KC_SPC,                             KC_RGUI, MO(_FL), KC_RALT, KC_RCTL),
+  KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,          UP_T, \
+  KC_LCTL, KC_LALT, KC_LGUI,                           KC_SPC,                             MO(_FL), LEFT_T,  DOWN_T,  RIGHT_T),
 
 /* Keymap _FL: Function Layer
    * ╭───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────╮
    * │ ~ │ F1│F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│  DEL  │
    * ├───┴─┬─┴┬──┴┬──┼──┬┴─┬─┴─┬─┴──┬┴──┬┴──┬┴───┼───┴┬──┴─┬─────┤
-   * │     │  │ Up│JM│GM│GH│HTP│HTPS│Ins│   │PrSc│Sclk│Paus│     │
+   * │     │  │   │JM│GM│GH│HTP│HTPS│Ins│   │PrSc│Sclk│Paus│     │
    * ├─────┴┬─┴──┬┴──┴┬─┴─┬┴──┬┴──┬─┴┬──┼───┼───┬┴───┬┴───┬┴─────┤
-   * │      │Left│Down│Rig│JMP│PIV│  │  │   │   │Home│PgUp│      │
+   * │      │    │    │   │JMP│PIV│  │  │   │   │Home│PgUp│      │
    * ├──────┴─┬──┴┬───┼──┬┴─┬─┴─┬─┴─┬┴──┼───┴┬──┴┬───┴┬───┴──────┤
    * │        │   │App│  │  │   │Vdn│Vup│Mute│End│PgDn│          │
    * ├────┬───┴┬──┴─┬─┴──┴──┴───┴───┴───┴────┼───┴┬───┴┬────┬────┤
@@ -70,8 +75,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = KEYMAP_ANSI(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
-  KC_TRNS, KC_TRNS, KC_UP,   JM23,    GMAIL,   GHUB,    HTTP,    HTTPS,    KC_INS,  KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, \
-  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, JMPIV,   PIVIO,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP,          KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, JM23,    GMAIL,   GHUB,    HTTP,    HTTPS,    KC_INS,  KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, JMPIV,   PIVIO,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP,          KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_APP,  KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU,  KC_MUTE, KC_END,  KC_PGDN,                   KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
