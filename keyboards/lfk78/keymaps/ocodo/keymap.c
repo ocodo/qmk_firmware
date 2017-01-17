9b5b76afac71c02c69a2e4e2c189dd3a2570d57f
jasonm23 <jasonm23@gmail.com>
2017-01-17 11:29:32 +0800

    Move shortcuts to 4key cluster
    ins / del -> home / end

--- keyboards/lfk78/keymaps/ocodo/keymap.c
+++ keyboards/lfk78/keymaps/ocodo/keymap.c
@@ -48,9 +48,9 @@ const Layer_Info layer_info[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap OCODO: (Ocodo base layer) Default Layer
    * ,---------.  ,------------------------------------------------------------.  ,---------.
-   * |    |    |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa|  | Ins|PgUp|
+   * |    |    |  |Esc~| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |Backspa|  |Home|PgUp|
    * |---------|  |------------------------------------------------------------|  |---------|
-   * |    |    |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     \|  | Del|PgDn|
+   * |    |    |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|     \|  |End |PgDn|
    * |---------|  |------------------------------------------------------------|  `---------'
    * |    |    |  |Ctrl  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return   |
    * |---------|  |------------------------------------------------------------|  ,----.
@@ -61,19 +61,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [OCODO] = KEYMAP(
-                   KC_NO,   KC_NO,    KC_FN1,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,      KC_MINS,   KC_EQL,     KC_BSPC,    KC_INS,   KC_PGUP,
-                   KC_NO,   KC_NO,    KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,      KC_LBRC,   KC_RBRC,    KC_BSLS,    KC_DEL,   KC_PGDN,
-                   KC_NO,   KC_NO,    BSP_CTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_QUOT,               RET_CTL,
-                   KC_NO,   KC_NO,    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,                           KC_UP,
-                   KC_NO,   KC_NO,    KC_LCTL,   KC_LALT,   KC_LGUI,              KC_SPC,                        KC_NO,  KC_LCTL,   MO(FUNC),               KC_LEFT,   KC_DOWN,   KC_RGHT),
+                   KC_NO,   KC_NO,    KC_FN1,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,   KC_0,      KC_MINS,   KC_EQL,     KC_BSPC,    KC_HOME,  KC_PGUP,
+                   KC_NO,   KC_NO,    KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,   KC_P,      KC_LBRC,   KC_RBRC,    KC_BSLS,    KC_END,   KC_PGDN,
+                   KC_NO,   KC_NO,    BSP_CTL,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,   KC_SCLN,   KC_QUOT,               RET_CTL,
+                   KC_NO,   KC_NO,    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH,   KC_RSFT,                           KC_UP,
+                   KC_NO,   KC_NO,    KC_LCTL,   KC_LALT, KC_LGUI,               KC_SPC,                          KC_NO,  KC_LCTL,   MO(FUNC),               KC_LEFT,   KC_DOWN,   KC_RGHT),
 
   /* Keymap FUNCTION: Function Layer
    * ,---------.  ,-------------------------------------------------------------.  ,---------.
-   * |    |    |  | ` |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|   Del   |  |    |Home|
+   * |    |    |  | ` |F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|   Del   |  |jm23|gml |
    * |---------|  |-------------------------------------------------------------|  |---------|
-   * |    |    |  |     |   |   |   |   |   |   |   |   |JM |GM |HTP|SSL| GHB   |  |    |End |
+   * |    |    |  |     |   |   |   |   |   |   |   |   |   |   |HTP|SSL| GHB   |  |jmil|piv |
    * |---------|  |-------------------------------------------------------------|  `---------'
-   * |    |    |  |      |   |   |   |   |   |   |   |   |   |JMI|PIV|          |
+   * |    |    |  |      |   |   |   |   |   |   |   |   |   |   |   |          |
    * |---------|  |-------------------------------------------------------------|  ,----.
    * |    |    |  |       |   |   |   |   |   |   |   |   |   |   | Tgl Numpad  |  |    |
    * |---------|  |--------------------------------------------------------------------------.
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------'  `-------------------------------------------------------'   `--------------'
    */
   [FUNC] = KEYMAP(
-                  KC_NO,    KC_NO,    KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,   KC_TR,   KC_HOME,
-                  KC_NO,    KC_NO,    KC_TR,    KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   JM23,    GMAIL,    HTTP,     HTTPS,    GHUB,     KC_TR,   KC_END,
-                  KC_NO,    KC_NO,    KC_TR,    KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   JMPIV,    PIVIO,              KC_TR,
+                  KC_NO,    KC_NO,    KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_DEL,   JM23,    GMAIL,
+                  KC_NO,    KC_NO,    KC_TR,    KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,    HTTP,     HTTPS,    GHUB,     JMPIV,   PIVIO,
+                  KC_NO,    KC_NO,    KC_TR,    KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,    KC_TR,              KC_TR,
                   KC_NO,    KC_NO,    KC_TR,    KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,   KC_TR,    TG(NUMPAD),                   KC_TR,
                   KC_NO,    KC_NO,    KC_TR,    KC_TR,   KC_TR,                     KC_TR,                              KC_TR,   KC_RALT,  KC_TR,               KC_TR,   KC_TR,   KC_TR),
 
