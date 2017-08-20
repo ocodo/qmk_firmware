922a49bb99b93143989e9c7fb1f725c0f7f879d9
Jasonm23 <jasonm23@gmail.com>
2017-08-20 13:54:09 +0800

    Fix tapdance config for nerd, handwired and phantom

--- keyboards/gonnerd/keymaps/tkl/keymap.c
+++ keyboards/gonnerd/keymaps/tkl/keymap.c
@@ -8,8 +8,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                    KC_ESC,           KC_F1,   KC_F2,  KC_F3, KC_F4,   KC_F5,  KC_F6, KC_F7,  KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,    KC_PSCR,KC_SLCK,KC_PAUS, \
                    __GUGR_,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  __GUBK_,   KC_INS, KC_HOME,KC_PGUP, \
                    __ALTB_,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC, KC_RBRC, __ALBS_,   KC_DEL, KC_END, KC_PGDN, \
-                   __CTBS_, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    TD_CL,   TD_QUOT, _x_,     __CTEN_,   \
-                   KC_LSFT, _x_,     KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _x_,               KC_UP,           \
+                   __CTBS_,  KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    TD_CL,     TD_QOT, _x_,     __CTEN_,   \
+                   KC_LSFT,  _x_,     KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _x_,               KC_UP,           \
                    KC_LCTL, KC_LALT, KC_LGUI,                         KC_SPC,                          KC_RGUI, KC_RALT, MO(1),   KC_RCTL,   KC_LEFT,KC_DOWN,KC_RGHT \
                     ),
   /* Layer 1 - Press Fn (App/Menu key) */
