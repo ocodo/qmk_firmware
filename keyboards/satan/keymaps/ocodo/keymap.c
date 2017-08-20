1ee021401c02b05d5bc3010f0fc8c6d53ac38f07
Jasonm23 <jasonm23@gmail.com>
2017-08-20 20:28:13 +0800

    add semicolon and quot tap dance to satan

--- keyboards/satan/keymaps/ocodo/keymap.c
+++ keyboards/satan/keymaps/ocodo/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = KEYMAP_ANSI(
                     F(20),   KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
                     KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-                    __CTBS_, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,          __CTEN_, \
+                    __CTBS_, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    TD_CL,   TD_QOT,           __CTEN_, \
                     KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,          UP_T,             \
                     KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                              MO(1),   LEFT_T,  DOWN_T,  RIGHT_T),
 
