c010b29719dec476b4c63d9008f32dab629f54e9
Jasonm23 <jasonm23@gmail.com>
2017-08-22 20:48:45 +0800

    Turn off tap dance on scln and quot

--- keyboards/frosty_flake/keymaps/default/keymap.c
+++ keyboards/frosty_flake/keymaps/default/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_ESC ,          KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,   KC_F7,   KC_F8,    KC_F9,  KC_F10,  KC_F11,  KC_F12,    KC_PSCR, KC_SLCK, KC_PAUS, \
                __GUGR_,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,    KC_8,    KC_9,     KC_0, KC_MINS,  KC_EQL, __GUBK_,     KC_INS, KC_HOME, KC_PGUP,    _x_,_x_,_x_,_x_, \
                __ALTB_,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,     KC_P, KC_LBRC, KC_RBRC, __ALBS_,     KC_DEL,  KC_END, KC_PGDN,    _x_,_x_,_x_,_x_, \
-               __CTBS_,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,    KC_K,    KC_L,    TD_CL,  TD_QOT,          __CTEN_,                                  _x_,_x_,_x_,     \
+               __CTBS_,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,          __CTEN_,                                  _x_,_x_,_x_,     \
                KC_LSFT,KC_NUBS,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,    KC_M, KC_COMM,   KC_DOT, KC_SLSH,          KC_RSFT,               KC_UP,             _x_,_x_,_x_,_x_, \
                KC_LCTL,KC_LALT,KC_LGUI,                 KC_SPC,                                    KC_RGUI, KC_RALT,   MO(1), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    _x_,    _x_)   ,
 
@@ -17,5 +17,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                _TAX,      KC_7,   KC_8,   KC_9,   __PL,   __MI,    _x_,    _x_,     _x_,     _x_,      _x_,     _x_,     _x_,     _x_,       _DEX,    _ENX,    _PDX,    _x_,_x_,_x_,_x_, \
                _CAX,      KC_4,   KC_5,   KC_6,   __SL,   __AS,    _x_,    _x_,     _x_,     _x_,      _x_,     _x_,              _x_,                                  _x_,_x_,_x_,     \
                _LSX,       _x_,   KC_1,   KC_2,   KC_3,   KC_0, KC_DOT,    _x_,     _x_,     _x_,      _x_,     _x_,              _x_,                _UAX,             _x_,_x_,_x_,_x_, \
-               _x_, _x_,KC_ENT,                            _x_,                                        _x_,     _x_,     _x_,  KC_APP,        _x_,    _DAX,    _x_,     _x_,    _x_)   ,
+               _x_,        _x_, KC_ENT,                    _x_,                                        _x_,     _x_,     _x_,  KC_APP,        _x_,    _DAX,    _x_,     _x_,    _x_)   ,
 };
--- keyboards/ocodo_phantom/keymaps/default/keymap.c
+++ keyboards/ocodo_phantom/keymaps/default/keymap.c
@@ -7,26 +7,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_ESC,             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,       KC_PSCR, KC_SLCK,  KC_BRK, \
          __GUGR_,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, __GUBK_,        KC_INS, KC_HOME, KC_PGUP, \
          __ALTB_,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, __ALBS_,        KC_DEL,  KC_END, KC_PGDN, \
-         __CTBS_,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   TD_CL,  TD_QOT,          __CTEN_, \
+         __CTBS_,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,          __CTEN_, \
          KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,                   KC_RSFT,                  KC_UP, \
          KC_LCTL, KC_LALT, KC_LGUI,                   __CTSP_,                                     KC_RGUI, KC_RALT,   MO(1), KC_RCTL,       KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Layer 1: FN0 - When FN is held down */
   KEYMAP(\
-         _ESX,               _NM_,    _EM_,    _GH_,    _HT_, _SSL_, _x_, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,       _PN_,    _PV_,    _PT_, \
-         _TIX,       KC_A,   KC_B,    KC_C,    KC_D,    KC_E,  KC_F, _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,       _INX,    _HOX,    _PUX, \
-         _TAX,       KC_7,   KC_8,    KC_9,    __PL,    __MI,   _x_, _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,       _DEX,    _ENX,    _PDX, \
-         _CAX,       KC_4,   KC_5,    KC_6,    __SL,    __AS,   _x_, _x_,     _x_,     _x_,     _x_,     _x_,              _x_, \
-         _LSX,       KC_1,   KC_2,    KC_3,    KC_0,    __DT,   _x_, _x_,     _x_,     _x_,     _x_,                       _x_,                _x_, \
-         _x_,         _x_,   __EN,                     TG(2),                                   _x_,     _x_,     _x_,  KC_APP,       _x_,     _x_,      _x_),
+            _ESX,            _NM_,    _EM_,    _GH_,    _HT_,   _SSL_,     _x_, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,          _PN_,    _PV_,    _PT_, \
+            _TIX,    KC_A,   KC_B,    KC_C,    KC_D,    KC_E,    KC_F,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,          _INX,    _HOX,    _PUX, \
+            _TAX,    KC_7,   KC_8,    KC_9,    __PL,    __MI,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,          _DEX,    _ENX,    _PDX, \
+            _CAX,    KC_4,   KC_5,    KC_6,    __SL,    __AS,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,              _x_, \
+            _LSX,    KC_1,   KC_2,    KC_3,    KC_0,    __DT,     _x_,     _x_,     _x_,     _x_,     _x_,                       _x_,                    _x_, \
+             _x_,     _x_,   __EN,                     TG(2),                                         _x_,     _x_,     _x_,  KC_APP,          _x_,      _x_,     _x_),
 
   /* Layer 2: Mouse keys (FN + Space to toggle) */
   KEYMAP(\
-         _x_,             _NM_,    _EM_,    _GH_,    _HT_,   _SSL_,    _PN_,    _PV_,    _PT_, _x_, _x_, _x_, _x_,       KC_BTN3, KC_BTN4, KC_BTN5, \
-         _x_,    KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,     _x_,     _x_,     _x_, _x_, _x_, _x_, _x_,       KC_WH_L, KC_WH_U, KC_WH_R, \
-         _x_,    KC_7,    KC_8,    KC_9,    __PL,    __MI,     _x_,     _x_,     _x_,     _x_, _x_, _x_, _x_, _x_,       KC_BTN1, KC_WH_D, KC_BTN2, \
-         _x_,    KC_4,    KC_5,    KC_6,    __SL,    __AS,     _x_,     _x_,     _x_,     _x_, _x_, _x_,      _x_, \
-         _x_,    KC_1,    KC_2,    KC_3,    KC_0,    __DT,     _x_,     _x_,     _x_,     _x_, _x_,           _x_,                KC_MS_U, \
-         _x_,     _x_,    __EN,                       _x_,                                     _x_, _x_, _x_, _x_,       KC_MS_L, KC_MS_D, KC_MS_R),
+            _x_,             _NM_,    _EM_,    _GH_,    _HT_,   _SSL_,    _PN_,    _PV_,    _PT_,     _x_,     _x_,     _x_,     _x_,      KC_BTN3,  KC_BTN4, KC_BTN5, \
+            _x_,     KC_A,   KC_B,    KC_C,    KC_D,    KC_E,    KC_F,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,      KC_WH_L,  KC_WH_U, KC_WH_R, \
+            _x_,     KC_7,   KC_8,    KC_9,    __PL,    __MI,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,      KC_BTN1,  KC_WH_D, KC_BTN2, \
+            _x_,     KC_4,   KC_5,    KC_6,    __SL,    __AS,     _x_,     _x_,     _x_,     _x_,     _x_,     _x_,              _x_, \
+            _x_,     KC_1,   KC_2,    KC_3,    KC_0,    __DT,     _x_,     _x_,     _x_,     _x_,     _x_,                       _x_,                KC_MS_U, \
+            _x_,      _x_,   __EN,                       _x_,                                         _x_,     _x_,     _x_,     _x_,      KC_MS_L,  KC_MS_D, KC_MS_R),
 
 };
