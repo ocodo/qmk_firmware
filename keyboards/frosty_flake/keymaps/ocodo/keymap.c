b2fe314f10f797fe6d0667d6d49411d5323b37a3
Jason Milkins <jasonm23@users.noreply.github.com>
2024-09-06 11:17:51 +0700

    Set alt and gui to their windows locations

--- keyboards/frosty_flake/keymaps/ocodo/keymap.c
+++ keyboards/frosty_flake/keymaps/ocodo/keymap.c
@@ -8,7 +8,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                __ALTB_,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,    KC_O,     KC_P, KC_LBRC, KC_RBRC, __ALBS_,     KC_DEL,  KC_END, KC_PGDN, \
                __CTBS_,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,    KC_K,    KC_L,  KC_SCLN, KC_QUOT,          __CTEN_, \
                KC_LSFT,KC_NUBS,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,    KC_M, KC_COMM,   KC_DOT, KC_SLSH,          KC_RSFT,               KC_UP, \
-               KC_LCTL,KC_LALT,KC_LGUI,                 KC_SPC,                                    KC_RGUI, KC_RALT,   MO(1), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT ),
+               KC_LCTL,KC_LGUI,KC_LALT,                 KC_SPC,                                    KC_RALT, KC_RGUI,    MO(1), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT ),
 
   [1] = KEYMAP_TKL(\
                _ESX,              _NM_,   _EM_,   _GH_,   _HT_,  _SSL_,    _x_, KC_MPRV, KC_MPLY,  KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,       _PN_,    _PV_,    _PT_, \
