032e3a31efdc4d3f7e8663a8677c2dfd989d826a
Jason Milkins <jasonm23@users.noreply.github.com>
2024-11-05 12:38:13 +0700

    Ocodo Frosty Flake N as KP_ENTER

--- keyboards/frosty_flake/keymaps/ocodo/keymap.c
+++ keyboards/frosty_flake/keymaps/ocodo/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                _x_,              _x_,    _x_,    _x_,    _x_,    _x_,     _x_, KC_MPRV, KC_MPLY,  KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,       KC_NLCK,_x_,    _x_, \
                _x_,      KC_A,   KC_B,   KC_C,   KC_D,   KC_E,   KC_F,    _x_,     _x_,     _x_,      _x_,     _x_,     _x_,     _x_,       _x_,    _x_,    _x_, \
                _x_,      KC_P7,  KC_P8,  KC_P9,  __PL,   __MI,    _x_,    _x_,     _x_,     _x_,      _x_,     _x_,     _x_,     _x_,       _x_,    _x_,    _x_, \
-               _x_,      KC_P4,  KC_P5,  KC_P6,   __SL,   __AS,    _x_,    _x_,   _KBD_,     _x_,      _x_,     _x_,              _x_, \
-               _x_,       _x_,   KC_P1,  KC_P2,  KC_P3,   KC_P0, KC_PDOT, _x_,     _x_,     _x_,      _x_,     _x_,              _x_,                _x_, \
-               _x_,        _x_,  KC_PENT,                 _x_,                                        _x_,     _x_,     _x_,  KC_APP,        _x_,    _x_,    _x_ )
+               _x_,      KC_P4,  KC_P5,  KC_P6,   __SL,   __AS,    _x_,    _x_,  _KBD_,     _x_,      _x_,     _x_,              _x_, \
+               _x_,       _x_,   KC_P1,  KC_P2,  KC_P3,   KC_P0, KC_PDOT, KC_PENT, _x_,     _x_,      _x_,     _x_,              _x_,                _x_, \
+               _x_,        _x_,  _x_,                   _x_,                                          _x_,     _x_,     _x_,  KC_APP,        _x_,    _x_,    _x_ )
 };
