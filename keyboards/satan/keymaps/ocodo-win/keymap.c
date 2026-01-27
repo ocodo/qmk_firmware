841f06956bf6447f5edaa4c525fbc4b62d1a3591
Jason Milkins <jasonm23@users.noreply.github.com>
2026-01-27 12:59:11 +0700

    Create ocodo-win keymap from ocodo keymap (macos)

--- keyboards/satan/keymaps/ocodo-win/keymap.c
+++ keyboards/satan/keymaps/ocodo-win/keymap.c
@@ -1,14 +1,20 @@
 #include "satan.h"
 #include "ocodo.c"
 
+#undef DOWN_T
+#undef LEFT_T
+
+#define DOWN_T    GUI_T(KC_DOWN)
+#define LEFT_T    ALT_T(KC_LEFT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer */
   [0] = KEYMAP_ANSI(
                     F(20),   KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    KC_6,    KC_7,     KC_8,    KC_9,      KC_0,  KC_MINS, KC_EQL,  KC_BSPC, \
                     KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,      KC_P,  KC_LBRC, KC_RBRC, KC_BSLS, \
-                    __CTBS_, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN,  KC_QUOT,          __CTEN_, \
+                    __CTBS_, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN,  KC_QUOT,          KC_ENT, \
                     KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,              UP_T,          \
-                    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                               MO(1),   LEFT_T,  DOWN_T,  RIGHT_T),
+                    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                                MO(1),   LEFT_T,  DOWN_T,  RIGHT_T),
 
   /* Keymap: function layer*/
   [1] = KEYMAP_ANSI(
