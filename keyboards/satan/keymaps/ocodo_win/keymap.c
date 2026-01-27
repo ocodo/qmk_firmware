044327acaa86f56f11a80035450023958f6b0950
jasonm23 <jasonm23@gmail.com>
2026-01-27 18:57:17 +0700

    Fix missing line continuation escapes

--- keyboards/satan/keymaps/ocodo_win/keymap.c
+++ keyboards/satan/keymaps/ocodo_win/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,     KC_T,      KC_Y,    KC_U,       KC_I,    KC_O,       KC_P,  KC_LBRC, KC_RBRC,  KC_BSLS, \
                     KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,     KC_G,      KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT, \
                     KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,     KC_B,      KC_N,    KC_M,    KC_COMM,  KC_DOT,    KC_SLSH,              UP_T,           \
-                    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                        MO(1),               LEFT_T,  DOWN_T,  RIGHT_T
+                    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                        MO(1),               LEFT_T,  DOWN_T,  RIGHT_T  \
                    ),
 
   /* Keymap: function layer*/
@@ -26,6 +26,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     _x_,     _x_,     _x_,     _x_,    _x_,      _x_,       _x_,     KC_INS,      _x_, KC_PSCR,    KC_SLCK,  KC_PAUS,     _x_,           \
                     _x_,     _x_,     _x_,     _x_,    _x_,      _x_,       _x_,     _x_,         _x_,     _x_,    KC_HOME,  KC_PGUP,               _x_, \
                     _x_,     _x_,     _x_,     _x_,    _x_,      _x_,       KC_VOLD, KC_VOLU, KC_MUTE,  KC_END,    KC_PGDN,               _x_,           \
-                    _x_,     _x_,     _x_,                                 _x_,                            _x_,              _x_,         _x_,      _x_    
+                    _x_,     _x_,     _x_,                                 _x_,                            _x_,              _x_,         _x_,      _x_  \  
                    ),
 };
